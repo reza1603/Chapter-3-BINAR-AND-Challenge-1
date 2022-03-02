5f1ef888ebb1c036d1427be4635817f59f8f49b0
Muhammad Reza Afriansyah <rezaafriansyah1603@gmail.com>
2022-03-02 19:52:11 +0700

    first commit

---
+++ app/src/main/java/and/binar/myfirstapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package and.binar.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val tvMyTextView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+        btnClickMe.setOnClickListener {
+            timesClicked = timesClicked + 1
+
+            tvMyTextView.text = timesClicked.toString()
+            Toast.makeText(this, "Hey, I'm a Reza btw!", Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
