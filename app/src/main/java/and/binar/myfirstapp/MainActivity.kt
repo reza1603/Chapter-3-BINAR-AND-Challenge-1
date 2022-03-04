16e174ce7076ed9ca3bbaea839c6a18500f8d0b4
Muhammad Reza Afriansyah <rezaafriansyah1603@gmail.com>
2022-03-04 19:49:42 +0700

    add comments

--- app/src/main/java/and/binar/myfirstapp/MainActivity.kt
+++ app/src/main/java/and/binar/myfirstapp/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //this is only comments
 
         val btnClickMe = findViewById<Button>(R.id.mybutton)
         val tvMyTextView = findViewById<TextView>(R.id.textView)
